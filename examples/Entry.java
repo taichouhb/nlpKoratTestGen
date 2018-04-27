792faaee4c4ffff129bf3dc2b78785818ec63417
Benjamin Cheung <bcheung@umass.edu>
2018-04-27 17:53:32 -0400

    Fixed another corner case, where if Entry is in the line, the keyword try gets flagged. Also added README showing how to compile java code to run with korat and how to run the parser

---
+++ examples/Entry.java
@@ -0,0 +1,13 @@
+public class Entry {
+
+    ListObject element;
+
+    Entry next;
+
+    Entry previous;
+
+    public String toString() {
+        return "[" + (element != null ? element.toString() : "null") + "]";
+    }
+
+}
--- parser.py
+++ parser.py
@@ -17,14 +17,14 @@ otherKeyWords = ["abstract", "assert", "break", "case", "catch", "class", "conti
 
 ignoredKeywords = ["public", "private"]
 
-def classVarHelper(startInput, customClassNames): 
+def classVarHelper(relPath, startInput, customClassNames): 
 	foundMainClass = False
 	startClass, startFile = startInput
 	stack = []
 	foundVars = {} #Map key: type, value: variable name
 	foundVars[startClass] = {}
 	customClassesFound = set() 
-	f = open(startFile + ".java", "r")
+	f = open(relPath + startFile + ".java", "r")
 	lines = f.readlines()
 	f.close()
 	i = 0
@@ -44,36 +44,41 @@ def classVarHelper(startInput, customClassNames):
 				stack.append("{")
 			elif c=="}":
 				stack.pop()	
-		
-		if len(stack) == 1 and len(line) > 3: #Current line of code is not in a method block. Should be in highest level of class block
+
+		if len(stack) == 1: #Current line of code is not in a method block. Should be in highest level of class block
 			#ie a class Variable? 
-	
-			relevant = True
+			relevant = True 
 			for w in otherKeyWords:
-				if w in line:
-					relevant = False
+				for t in line.split(" "):
+					if t==w or t=="{":
+						relevant = False
+					
+
 			if relevant == True:
+				# print(line)
 				line = line.strip()
 				if line.startswith("public"):
 					line = line[6:]
 				if line.startswith("private"):
 					line = line[7:]
 				tokens = line.strip().rstrip(";").split(" ")
-
-				if tokens[0] in [i[0] for i in customClassNames]:
-					customClassesFound.add(tokens[0])
+				if len(tokens) > 1:
+					# print(line)
+					if tokens[0] in [i[0] for i in customClassNames]:
+						customClassesFound.add(tokens[0])
+					
+					if tokens[0] not in foundVars[startClass]:
+						foundVars[startClass][tokens[0]] = []
 				
-				if tokens[0] not in foundVars[startClass]:
-					foundVars[startClass][tokens[0]] = []
-				foundVars[startClass][tokens[0]].append(tokens[1])
+					foundVars[startClass][tokens[0]].append(tokens[1])
 		i+=1
 	return (foundVars, customClassesFound)
 
 #start Input: (startClass, startFile)
 #customClassNames: [(Class, File), (Class, File), etc]
-def parseClassVariables(startInput, customClassNames):
+def parseClassVariables(relPath, startInput, customClassNames):
 	
-	foundVars, customClassesFound = classVarHelper(startInput, customClassNames)
+	foundVars, customClassesFound = classVarHelper(relPath, startInput, customClassNames)
 	#Now, iterate through the list of customClasses and find the variables in there.
 	listed = list(customClassesFound)
 	classToFile = dict(customClassNames)
@@ -81,7 +86,7 @@ def parseClassVariables(startInput, customClassNames):
 	while len(listed) > 0:
 		className = listed.pop(0) #remove first element in list
 		foundVars[className] = {}
-		i_found, i_set = classVarHelper( (className, classToFile[className]), customClassNames )
+		i_found, i_set = classVarHelper(relPath, (className, classToFile[className]), customClassNames )
 		foundVars[className] = i_found[className]
 		for i in i_set:
 			if i not in customClassesFound:
@@ -94,16 +99,24 @@ def parseClassVariables(startInput, customClassNames):
 
 def main():
 	#"Class.Filename"
-	inp = sys.argv[1].split(".")
+	inp = sys.argv[2].split(".")
 	startInput = (inp[0], inp[1])
 	arr = []
-	for val in sys.argv[2:]:
+	for val in sys.argv[3:]:
 		vals = val.split(".")
 		arr.append((vals[0], vals[1]))
-	print(parseClassVariables(startInput, arr))
+	print(parseClassVariables(sys.argv[1], startInput, arr))
 
 
 if __name__ == "__main__":
 	main()
 
-#python parser.py RaceFreeTest.RaceFreeTest threadThing.RaceFreeTest syncObject.RaceFreeTest
+#python parser.py ./ RaceFreeTest.RaceFreeTest threadThing.RaceFreeTest syncObject.RaceFreeTest
+
+#output
+#{'syncObject': {'int': ['value']}, 'RaceFreeTest': {'String': ['hat'], 'boolean': ['tree'], 'syncObject': ['once', 'twice']}}
+
+#python parser.py examples/ DoublyLinkedList.DoublyLinkedList Entry.Entry ListObject.ListObject
+
+#output
+#({'DoublyLinkedList': {'int': ['size'], 'Entry': ['header']}, 'Entry': {'Entry': ['next', 'previous'], 'ListObject': ['element']}, 'ListObject': {'int': ['myID']}}, {'DoublyLinkedList', 'Entry', 'ListObject'})
