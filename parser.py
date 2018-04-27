460fd8639b81dbdd68b9b384a8d5fd3e68dede7a
Benjamin Cheung <bcheung@umass.edu>
2018-04-27 12:31:43 -0400

    Finished the classVariablesParser

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import sys
 #MUST ALSO INPUT ALL CUSTOM CLASSES THE SYSTEM USES AFTER THE FILENAME
-
+import pdb
 
 primitiveList = ["boolean", "byte", "char", "double", "int", "long", "short", "float"]
 # otherKeyWords = ["abstract", "assert", "break", "case", "catch", "class", "continue", \
@@ -11,32 +11,63 @@ primitiveList = ["boolean", "byte", "char", "double", "int", "long", "short", "f
 
 otherKeyWords = ["abstract", "assert", "break", "case", "catch", "class", "continue", \
 				"default", "do", "else", "enum", "final", "finally", "for", "if", \
-				"instanceof", "interface", "native", "new", "package", "private", "protected", \
-				"public", "return", "static", "super", "strictfp", "switch", "synchronized", \
+				"instanceof", "interface", "native", "new", "package", "protected", \
+				"return", "static", "super", "strictfp", "switch", "synchronized", \
 				"this", "throw", "throws", "transient", "try", "void", "volatile", "while"]
+
+ignoredKeywords = ["public", "private"]
 
 
 def parseClassVariables(startClass, customClassNames):
 	foundMainClass = False
 	stack = []
 	foundVars = {} #Map key: type, value: variable name
+	foundVars[startClass] = {}
+	classesFound = [] 
+	f = open(startClass + ".java", "r")
+	lines = f.readlines()
+	f.close()
+	print("reading file")
+	i = 0
+	while foundMainClass==False and i < len(lines):
+		line = lines[i]
+		# print(line)	
+		if "class " + startClass in line:
+			foundMainClass = True
+			stack.append("{")
+		i+=1
+	print("found main class")
+	while len(stack) > 0 and i < len(lines):#we're already within the main class definition. Now look for class variables
+	#This keeps checking only within the main class block. 
+		line = lines[i]
+		# print(line)
+		if "{" in line: #TODO: Corner case, where a line may have more than 1 of these
+			stack.append("{")
+		if "}" in line: #TODO: Corner case, where a line may have more than 1 of these
+			stack.pop()
+		if len(stack) == 1 and len(line) > 3: #Current line of code is not in a method block. Should be in highest level of class block
+			#ie a class Variable? 
+			print(line)
+			relevant = True
+			for w in otherKeyWords:
+				if w in line:
+					relevant = False
+			if relevant == True:
+				line = line.strip()
+				if line.startswith("public"):
+					line = line[6:]
+				if line.startswith("private"):
+					line = line[7:]
+				tokens = line.strip().rstrip(";").split(" ")
+				if tokens[0] not in primitiveList:
+					classesFound.append(tokens[0])
+				# print(tokens)
+				if tokens[0] not in foundVars[startClass]:
+					foundVars[startClass][tokens[0]] = []
+				foundVars[startClass][tokens[0]].append(tokens[1])
+		i+=1
+	return foundVars, classesFound
 
-	with open(startClass + ".java", "r") as f:
-		for line in f:
-			if foundMainClass==False:
-				if line.contains("class startClass"):
-					foundMainClass = True
-					stack.append("{")
-			elif len(stack) > 0:#we're already within the main class definition. Now look for class variables
-			#This keeps checking only within the main class block. 
-				#Todo: Find variables
-
-				if line.contains("{"):
-					stack.append("{")
-				if line.contains("}"):
-					stack.pop()
-				if len(stack) == 1: #Current line of code is not in a method block. Should be in highest level of class block
-					#ie a class Variable? 
 
 
 
@@ -56,5 +87,5 @@ def main():
 	print(parseClassVariables(sys.argv[1], sys.argv[2:]))
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 	main()
